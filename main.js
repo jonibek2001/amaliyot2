991e83e75e830bb4544f7b3ddc307ad4e11a3d25
jonibek2001 <jorayevj100@gmial.com>
2023-02-04 19:25:17 +0500

    hero

---
+++ main.js
@@ -0,0 +1,116 @@
+ //amaliyot
+// let ism = prompt("ismingiz nima")
+
+// let birinchi = ism.charAt().toUpperCase();
+// let davomi = ism.slice(1).toLocaleLowerCase();
+
+// let result = birinchi + davomi;
+
+// alert(`salom, ${result}`)
+
+// for loop
+
+// for (let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+// let names = ['doni', 'umar', 'senator']
+
+// for (let i = 0; i < names.length; i++){
+//     console.log(names[i]);
+// }
+
+// let names = ['aslam', 'beckore', 'senator', 'umar']
+
+// for (let i = 0; i < names.length; i++){
+//     console.log(names[i]);
+// }
+
+// let parol = 'menin'
+
+// if (parol.length >= 9){
+//     console.log('siz togri yoldasiz');
+// } else if(parol.length >= 8){
+//     console.log('super');
+// }else{
+//     console.log('qayta urinib koring');
+// }
+
+// let baho = "g"
+
+// switch (baho){
+//     case "a":
+//         console.log("sizning bahongiz a");
+//         break
+//     case "b":
+//         console.log("sizning bahongiz b");
+//         break
+//     case "c":
+//         console.log("sizning bahongiz c");
+//         break
+//     default:
+//         console.log("good bye");            
+// }
+
+//global scope 
+// let hello = 'london'
+
+// if(true){
+//     let hello = "buxoro"
+//     console.log(hello);
+// }
+// console.log(hellokk);
+// let salom = prompt("sizning ismingiz nima");
+
+// let first = salom.charAt().toUpperCase();
+// let second = salom.slice(1).toLowerCase();
+
+// let result = first + second;
+
+// alert(`salom ${result}`)
+
+
+// for (let i = 0; i < 10; i++){
+//     console.log('salom' + i);
+// }
+// let salom = "merlinaaaaaa"
+
+// if(salom.length > 10){
+//     console.log("hello");
+// }else if(salom.length > 8){
+//     console.log("bye");
+// }else{
+//     console.log("polli");
+// }
+
+// let natija = [1, 12, 0, 90, 100]
+
+// for (let i = 0; i < natija.length; i++){
+//     if(natija[i] === 0){
+//         continue
+//     }
+//      console.log(natija[i], "full");
+
+//     if(natija[i] === 90){
+//         console.log("qale bratam");
+//         break
+//     }
+// }
+// let baho = "b"
+
+// switch(baho){
+//     case "a":
+//         console.log("helllo");
+//         break
+//     case "b":
+//         console.log("xayr");
+//         break    
+// }
+
+// let boy = "samandar"
+
+// if(true){
+//     let boy = "jonibek"
+//     console.log(boy);
+// }
+// console.log(boy);
